5c41d50e7c67babcd5ecd13f70eada45899262fb
Charles Mulder <charlesmulder@protonmail.com>
2019-03-25 18:42:36 +0200

    sticking to random colors as equal hue ranges did not do the trick

--- game.js
+++ game.js
@@ -82,6 +82,9 @@ const ding = "d3";
 const gu = "d5";
 const notes = ["c4", "d4", "e4", "bb3", ding, "f4", "a3", gu, "a4"];
 
+//var availHueRanges = [];
+//var takenHueRanges = [];
+
 /**
  * Gubal
  * The Ringding is tuned to four partial tones: E♭3, B♭3, E♭4 and G5. This way it serves a double function: It is a sound source itself as well as the place to excite the bass sound of the Helmholtz resonance.[6]
@@ -92,14 +95,6 @@ const notes = ["c4", "d4", "e4", "bb3", ding, "f4", "a3", gu, "a4"];
 //const ding = ["eb3", "bb3", "g5"];
 //const gu = ["eb2", "bb3"];
 //const notes = [["bb3", "f5"], ["c4", "g5"], ["db4", "ab5"], ["eb4", "bb5"], ding, ["f4", "c6"], ["g4", "eb6"], gu, ["bb4", "eb6"]];
-
-function isChord(data) {
-    return Array.isArray(data);
-}
-
-function isNote(data) {
-    return typeof data === 'string';
-}
 
 PS.init = function( system, options ) {
 	"use strict"; // Do not remove this directive!
@@ -116,7 +111,6 @@ PS.init = function( system, options ) {
      * @param {string} channel id
      */
     function loadNote(instrument, note, volume, callback) {
-        console.log('loading note', note, 'with volume', volume);
         PS.audioLoad(`${instrument}_${note}`, {
             volume: volume,
             lock: true,
@@ -135,7 +129,6 @@ PS.init = function( system, options ) {
      * @param {string[]} channel ids
      */
     function loadChord(instrument, chord, callback) {
-        console.log('loading chord', chord);
         let channelIds = [];
         let volume = 1;
         chord.forEach(note => {
@@ -148,7 +141,6 @@ PS.init = function( system, options ) {
     }
 
     function isAllSoundsLoaded(notes, loaded, callback) {
-        console.log('all sounds loaded?', notes, loaded, (loaded == notes.length));
         if(loaded == notes.length) {
             callback();
         }
@@ -164,7 +156,6 @@ PS.init = function( system, options ) {
             let row = calcRow(index);
             let color = calcColor(col, row, colors);
             if(isChord(note)) {
-                console.log('gu');
                 loadChord(instrument, note, (channelIds) => {
                     PS.color(col, row, color);
                     //PS.glyph(col, row, icons[row*3+col]);
@@ -199,21 +190,6 @@ PS.init = function( system, options ) {
     }
 
     /**
-     * Calculate column (x)
-     */
-    function calcColumn(index) {
-        return index % 3;
-    }
-
-
-    /**
-     * Calculate row (y)
-     */
-    function calcRow(index) {
-        return Math.floor(index/3);
-    }
-
-    /**
      * Calculate color (hex)
      *
      * @param {number} column
@@ -222,7 +198,7 @@ PS.init = function( system, options ) {
      * @return {hex} color
      */
     function calcColor(column, row, colors) {
-        return colors[row*3+column];
+        return colors[calcIndex(column, row)];
     }
 
     function loading() {
@@ -261,6 +237,71 @@ This function doesn't have to do anything. Any value returned is ignored.
 // UNCOMMENT the following code BLOCK to expose the PS.touch() event handler:
 
 /**
+ * hue ranges to choose from 360/15 = 24
+ *
+ * if (hue > 0)
+ * currentHue = beadHue;
+ *
+ * get list of avail hue ranges
+ * choose random hue range from list of avail hue ranges
+ * remove chosen hue range from avail hues
+ * add chosen hue range to taken hue ranges
+ * 
+ * if(currentHue)
+ *   determine hue range current hue belongs to
+ *   add hue range back to avail hue ranges
+ *
+ * choose random hue from chosen hue range
+ */
+function chooseHue(currentHue) {
+    if( ! takenHueRanges.length) {
+        availHueRanges = calcAllHueRanges();
+    }
+    let randomHueRange = chooseRandomValue(availHueRanges);
+    takenHueRanges.push(availHueRanges.splice(availHueRanges.indexOf(randomHueRange), 1)[0]);
+
+    if(currentHue) {
+        let currentHueRange = calcHueRange(currentHue, takenHueRanges);
+        availHueRanges.push(currentHueRange);
+        takenHueRanges.splice(takenHueRanges.indexOf(currentHueRange), 1);
+    }
+    return (randomHueRange+(PS.random(36)-1));
+}
+
+function chooseRandomValue(arr) {
+    return arr[PS.random(arr.length)-1];
+}
+
+function calcAllHueRanges() {
+    let hues = [];
+    for(x=0; x<360; x+=36) {
+        hues.push(x);
+    }
+    return hues;
+}
+
+/**
+ * determine hue range current hue belongs to
+ */
+function calcHueRange(hue, takenHueRanges) {
+    let differences = takenHueRanges.map( takenHueRange => {
+        return Math.abs(takenHueRange - hue);
+    });
+    let hueRangeIndex = differences.reduce((previousIndex, currentValue, currentIndex, differences) => {
+        if( ! currentIndex) {
+            return currentIndex;
+        }
+        if(differences[previousIndex] < differences[currentIndex]) {
+            return previousIndex;
+        } else {
+            return currentIndex;
+        }
+    }, 0);
+    return takenHueRanges[hueRangeIndex];
+}
+
+
+/**
  * Adjust saturation of colors when touched
  * http://www.niwa.nu/2013/05/math-behind-colorspace-conversions-rgb-hsl/
  */
@@ -268,10 +309,15 @@ PS.touch = function( x, y, data, options ) {
 	"use strict"; // Do not remove this directive!
     if(isNote(data)) { 
         PS.audioPlayChannel(data);
-        //if((previous.length && (previous[0] != x || previous[1] != y)) || !previous.length) {
-        PS.color(x, y, PS.random(255), PS.random(255), PS.random(255));
+        //PS.unmakeRGB(PS.color(x, y), currentRgbColor);
+        //let currentHsvColor = rgbToHsv(...currentRgbColor);
+        //let currentHue = currentHsvColor[0];
+        //let newHue = chooseHue(currentHue);
+        //let newRgbColor = hsvToRgb(newHue, PS.random(100)/100, (PS.random(50)+50)/100);
+        //PS.color(x, y, newRgbColor);
+        PS.color(x, y, [PS.random(255), PS.random(255), PS.random(255)]);
         PS.glyph(x, y, icons[PS.random(9)-1]);
-        //}
+
     }
     if(isChord(data)) {
         // might need to use promises here
@@ -279,202 +325,102 @@ PS.touch = function( x, y, data, options ) {
             console.log('playing channelId', note);
             PS.audioPlayChannel(note);
         });
-        //if((previous.length && (previous[0] != x || previous[1] != y)) || !previous.length) {
-        //PS.gridShadow(true, PS.random(255));
         PS.color(x, y, PS.random(255), PS.random(255), PS.random(255));
         PS.glyph(x, y, icons[PS.random(9)-1]);
-        //}
     }
-    //previous = [x, y];
-
 };
 
-/*
-PS.release ( x, y, data, options )
-Called when the left mouse button is released, or when a touch is lifted, over bead(x, y).
-This function doesn't have to do anything. Any value returned is ignored.
-[x : Number] = zero-based x-position (column) of the bead on the grid.
-[y : Number] = zero-based y-position (row) of the bead on the grid.
-[data : *] = The JavaScript value previously associated with bead(x, y) using PS.data(); default = 0.
-[options : Object] = A JavaScript object with optional data properties; see API documentation for details.
-*/
-
-PS.release = function( x, y, data, options ) {
-	"use strict"; // Do not remove this directive!
-
-	// Uncomment the following code line to inspect x/y parameters:
-
-    //PS.debug( "PS.release() @ " + x + ", " + y + "\n" );
-
-	// Add code here for when the mouse button/touch is released over a bead.
+function isChord(data) {
+    return Array.isArray(data);
 }
 
-/*
-PS.enter ( x, y, button, data, options )
-Called when the mouse cursor/touch enters bead(x, y).
-This function doesn't have to do anything. Any value returned is ignored.
-[x : Number] = zero-based x-position (column) of the bead on the grid.
-[y : Number] = zero-based y-position (row) of the bead on the grid.
-[data : *] = The JavaScript value previously associated with bead(x, y) using PS.data(); default = 0.
-[options : Object] = A JavaScript object with optional data properties; see API documentation for details.
-*/
+function isNote(data) {
+    return typeof data === 'string';
+}
 
-// UNCOMMENT the following code BLOCK to expose the PS.enter() event handler:
+/**
+ * Calculate column (x)
+ */
+function calcColumn(index) {
+    return index % 3;
+}
 
-/*
+/**
+ * Calculate row (y)
+ */
+function calcRow(index) {
+    return Math.floor(index/3);
+}
 
-PS.enter = function( x, y, data, options ) {
-	"use strict"; // Do not remove this directive!
+function calcIndex(column, row) {
+    return row*3+column;
+}
 
-	// Uncomment the following code line to inspect x/y parameters:
+/**
+ * Converts an RGB color value to HSV. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSV_color_space.
+ * Assumes r, g, and b are contained in the set [0, 255] and
+ * returns h, s, and v in the set [0, 1].
+ *
+ * @param   Number  r       The red color value
+ * @param   Number  g       The green color value
+ * @param   Number  b       The blue color value
+ * @return  Array           The HSV representation
+ */
+function rgbToHsv(r, g, b) {
+  r /= 255, g /= 255, b /= 255;
 
-	// PS.debug( "PS.enter() @ " + x + ", " + y + "\n" );
+  var max = Math.max(r, g, b), min = Math.min(r, g, b);
+  var h, s, v = max;
 
-	// Add code here for when the mouse cursor/touch enters a bead.
-};
+  var d = max - min;
+  s = max == 0 ? 0 : d / max;
 
-*/
+  if (max == min) {
+    h = 0; // achromatic
+  } else {
+    switch (max) {
+      case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+      case g: h = (b - r) / d + 2; break;
+      case b: h = (r - g) / d + 4; break;
+    }
 
-/*
-PS.exit ( x, y, data, options )
-Called when the mouse cursor/touch exits bead(x, y).
-This function doesn't have to do anything. Any value returned is ignored.
-[x : Number] = zero-based x-position (column) of the bead on the grid.
-[y : Number] = zero-based y-position (row) of the bead on the grid.
-[data : *] = The JavaScript value previously associated with bead(x, y) using PS.data(); default = 0.
-[options : Object] = A JavaScript object with optional data properties; see API documentation for details.
-*/
+    h *= 60;
+    //h /= 6;
+  }
 
-// UNCOMMENT the following code BLOCK to expose the PS.exit() event handler:
+  return [ h, s, v ];
+}
 
-PS.exit = function( x, y, data, options ) {
-	"use strict"; // Do not remove this directive!
+/**
+ * Converts an HSV color value to RGB. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSV_color_space.
+ * Assumes h, s, and v are contained in the set [0, 1] and
+ * returns r, g, and b in the set [0, 255].
+ *
+ * @param   Number  h       The hue
+ * @param   Number  s       The saturation
+ * @param   Number  v       The value
+ * @return  Array           The RGB representation
+ */
+function hsvToRgb(h, s, v) {
+  var r, g, b;
 
-	// Uncomment the following code line to inspect x/y parameters:
+  var i = Math.floor(h / 60);
+  var f = h / 60 - i;
+  var p = v * (1 - s);
+  var q = v * (1 - f * s);
+  var t = v * (1 - (1 - f) * s);
 
-	// Add code here for when the mouse cursor/touch exits a bead.
-};
+  switch (i % 6) {
+    case 0: r = v, g = t, b = p; break;
+    case 1: r = q, g = v, b = p; break;
+    case 2: r = p, g = v, b = t; break;
+    case 3: r = p, g = q, b = v; break;
+    case 4: r = t, g = p, b = v; break;
+    case 5: r = v, g = p, b = q; break;
+  }
 
-/*
-PS.exitGrid ( options )
-Called when the mouse cursor/touch exits the grid perimeter.
-This function doesn't have to do anything. Any value returned is ignored.
-[options : Object] = A JavaScript object with optional data properties; see API documentation for details.
-*/
+  return [ r * 255, g * 255, b * 255 ];
+}
 
-// UNCOMMENT the following code BLOCK to expose the PS.exitGrid() event handler:
-
-PS.exitGrid = function( options ) {
-	"use strict"; // Do not remove this directive!
-
-	// Uncomment the following code line to verify operation:
-
-    //PS.debug( "PS.exitGrid() called\n" );
-
-	// Add code here for when the mouse cursor/touch moves off the grid.
-};
-
-/*
-PS.keyDown ( key, shift, ctrl, options )
-Called when a key on the keyboard is pressed.
-This function doesn't have to do anything. Any value returned is ignored.
-[key : Number] = ASCII code of the released key, or one of the PS.KEY_* constants documented in the API.
-[shift : Boolean] = true if shift key is held down, else false.
-[ctrl : Boolean] = true if control key is held down, else false.
-[options : Object] = A JavaScript object with optional data properties; see API documentation for details.
-*/
-
-// UNCOMMENT the following code BLOCK to expose the PS.keyDown() event handler:
-
-/*
-
-PS.keyDown = function( key, shift, ctrl, options ) {
-	"use strict"; // Do not remove this directive!
-
-	// Uncomment the following code line to inspect first three parameters:
-
-	// PS.debug( "PS.keyDown(): key=" + key + ", shift=" + shift + ", ctrl=" + ctrl + "\n" );
-
-	// Add code here for when a key is pressed.
-};
-
-*/
-
-/*
-PS.keyUp ( key, shift, ctrl, options )
-Called when a key on the keyboard is released.
-This function doesn't have to do anything. Any value returned is ignored.
-[key : Number] = ASCII code of the released key, or one of the PS.KEY_* constants documented in the API.
-[shift : Boolean] = true if shift key is held down, else false.
-[ctrl : Boolean] = true if control key is held down, else false.
-[options : Object] = A JavaScript object with optional data properties; see API documentation for details.
-*/
-
-// UNCOMMENT the following code BLOCK to expose the PS.keyUp() event handler:
-
-/*
-
-PS.keyUp = function( key, shift, ctrl, options ) {
-	"use strict"; // Do not remove this directive!
-
-	// Uncomment the following code line to inspect first three parameters:
-
-	// PS.debug( "PS.keyUp(): key=" + key + ", shift=" + shift + ", ctrl=" + ctrl + "\n" );
-
-	// Add code here for when a key is released.
-};
-
-*/
-
-/*
-PS.input ( sensors, options )
-Called when a supported input device event (other than those above) is detected.
-This function doesn't have to do anything. Any value returned is ignored.
-[sensors : Object] = A JavaScript object with properties indicating sensor status; see API documentation for details.
-[options : Object] = A JavaScript object with optional data properties; see API documentation for details.
-NOTE: Currently, only mouse wheel events are reported, and only when the mouse cursor is positioned directly over the grid.
-*/
-
-// UNCOMMENT the following code BLOCK to expose the PS.input() event handler:
-
-/*
-
-PS.input = function( sensors, options ) {
-	"use strict"; // Do not remove this directive!
-
-	// Uncomment the following code lines to inspect first parameter:
-
-//	 var device = sensors.wheel; // check for scroll wheel
-//
-//	 if ( device ) {
-//	   PS.debug( "PS.input(): " + device + "\n" );
-//	 }
-
-	// Add code here for when an input event is detected.
-};
-
-*/
-
-/*
-PS.shutdown ( options )
-Called when the browser window running Perlenspiel is about to close.
-This function doesn't have to do anything. Any value returned is ignored.
-[options : Object] = A JavaScript object with optional data properties; see API documentation for details.
-NOTE: This event is generally needed only by applications utilizing networked telemetry.
-*/
-
-// UNCOMMENT the following code BLOCK to expose the PS.shutdown() event handler:
-
-/*
-
-PS.shutdown = function( options ) {
-	"use strict"; // Do not remove this directive!
-
-	// Uncomment the following code line to verify operation:
-
-	// PS.debug( "“Dave. My mind is going. I can feel it.”\n" );
-
-	// Add code here to tidy up when Perlenspiel is about to close.
-};
-
-*/
